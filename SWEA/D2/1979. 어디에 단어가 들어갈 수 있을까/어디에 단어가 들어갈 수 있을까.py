0be5b0c48af037dc4b121707dcf410dc3bc69ff7
stooiyaren <156271965+stooiyaren@users.noreply.github.com>
2024-01-31 11:54:33 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 151 ms, Memory: 59,596 KB -BaekjoonHub

---
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -0,0 +1,24 @@
+T = int(input())
+for t in range(1,T+1):
+    N,K = map(int,input().split())
+    matrix = [[0]*(N+2)]*(N+2)
+    answer = [0] + [1]*K +[0]
+    for i in range(1,N+1):
+        n = list(map(int,input().split()))
+        matrix[i] = [0]+ n +[0]
+    cnt = 0
+    for i in range(N-K+4):
+        for j in range(N-K+1):
+            lst = []
+            for k in range(K+2):
+                lst.append(matrix[i][j+k])
+            if lst == answer:
+                cnt +=1
+    for i in range(N-K+1):
+        for j in range(N-K+4):
+            lst = []
+            for k in range(K+2):
+                lst.append(matrix[i+k][j])
+            if lst == answer:
+                cnt +=1
+    print(f'#{t} {cnt}')
