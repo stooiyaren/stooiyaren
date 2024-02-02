fd38e715d02fb9a7b84e407a9a9953501632432d
stooiyaren <156271965+stooiyaren@users.noreply.github.com>
2024-02-02 11:09:26 +0900

    [D2] Title: 풍선팡, Time: 305 ms, Memory: 60,120 KB -BaekjoonHub

--- SWEA/D2/9490. 풍선팡/풍선팡.py
+++ SWEA/D2/9490. 풍선팡/풍선팡.py
@@ -1,21 +1,21 @@
+di = [0,1,0,-1]
+dj = [1,0,-1,0]
+
 T = int(input())
-for t in range(1,T+1):
-    N, M = map(int,input().split())
-    matrix = [list(map(int,input().split())) for _ in range(N)]
-    di = [-1,0,0,1]
-    dj = [0,-1,1,0]
-    pollen = 0
-    best = 0
+for tc in range(1,T+1):
+    N,M = map(int,input().split())
+    arr = [list(map(int,input().split())) for _ in range(N)]
+
+    max_v = 0
     for i in range(N):
         for j in range(M):
-            pollen = matrix[i][j]
-            if pollen ==0:
-                continue
-            else:
-                for p in range(1,pollen+1):
-                    for q in range(4):
-                        if 0<= (i+di[q]*p) < N and 0<= (j+dj[q]*p) < M:
-                            pollen += matrix[i+di[q]*p][j+dj[q]*p]
-                    if pollen > best:
-                        best = pollen
-    print(f'#{t} {best}')
+            cnt = arr[i][j]
+            for k in range(4):
+                for l in range(1, arr[i][j]+1):
+                    ni = i+di[k]*l
+                    nj = j+dj[k]*l
+                    if 0<= ni <N and 0<= nj < M:
+                        cnt += arr[ni][nj]
+            if max_v < cnt:
+                max_v = cnt
+    print(f'#{tc} {max_v}')      
