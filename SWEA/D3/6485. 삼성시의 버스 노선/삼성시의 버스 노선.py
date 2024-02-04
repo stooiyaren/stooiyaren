6494a5158cda0511bc4deb10e081718a58e3637c
stooiyaren <156271965+stooiyaren@users.noreply.github.com>
2024-02-05 00:02:03 +0900

    [D3] Title: 삼성시의 버스 노선, Time: 197 ms, Memory: 61,308 KB -BaekjoonHub

--- SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.py
+++ SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.py
@@ -1,15 +1,14 @@
 T = int(input())
-for i in range(1,T+1):
+for tc in range(1,T+1):
     N = int(input())
-    count = [0] * 5001
-    answer = []
-    for p in range(N):
-        a, b = map(int,input().split())
-        for q in range(a,b+1):
-            count[q] +=1
+    bus_stop = [0] *5001 
+    for i in range(N):
+        bus = list(map(int,input().split()))
+        for j in range(bus[0],bus[1]+1):
+            bus_stop[j] += 1
 
     P = int(input())
-    for k in range(P):
-        j = int(input())
-        answer.append(count[j])
-    print(f'#{i}', *answer)
+    print(f'#{tc}',end = ' ')
+    for i in range(P):
+        print(bus_stop[int(input())], end = ' ')
+    print()
