ed94807510c6c9d8ace45dcc894a6127118b9b77
stooiyaren <156271965+stooiyaren@users.noreply.github.com>
2024-02-14 15:37:35 +0900

    [D2] Title: 파리퇴치3, Time: 193 ms, Memory: 62,780 KB -BaekjoonHub

--- SWEA/D2/12712. 파리퇴치3/파리퇴치3.py
+++ SWEA/D2/12712. 파리퇴치3/파리퇴치3.py
@@ -1,35 +1,33 @@
-di = [0,0,1,-1]
-dj = [-1,1,0,0]
-ai = [1,1,-1,-1]
-aj = [1,-1,1,-1]
+dx = [-1,0,0,1]
+dy = [0,-1,1,0]
+
+rx = [1,1,-1,-1]
+ry = [-1,1,-1,1]
 
 T = int(input())
 for tc in range(1,T+1):
     N, M = map(int,input().split())
-
     matrix = [list(map(int,input().split())) for _ in range(N)]
-
-    mx_fly = 0
-
-
+    best = 0
     for i in range(N):
         for j in range(N):
+            
             pfly = matrix[i][j]
             xfly = matrix[i][j]
-
-            for p in range(4):
-                for q in range(1,M):
-                    pi = i +di[p]*q
-                    pj = j +dj[p]*q
-                    xi = i +ai[p]*q
-                    xj = j +aj[p]*q
-
-                    if 0<= pi and pi < N and 0 <= pj and pj < N:
-                        pfly += matrix[pi][pj]
-
-                    if 0 <= xi and xi <N and 0 <= xj and xj < N:
-                        xfly += matrix[xi][xj]
-                        
-            if mx_fly < max(pfly, xfly):
-                mx_fly = max(pfly, xfly)
-    print(f'#{tc} {mx_fly}')
+            for k in range(1,M):
+                for l in range(4):
+                    
+                    nx = i + dx[l]*k
+                    ny = j + dy[l]*k
+                    
+                    tx = i + rx[l]*k
+                    ty = j + ry[l]*k
+                    
+                    if 0 <= nx < N and 0 <= ny < N:
+                        pfly += matrix[nx][ny]
+                    if 0 <= tx < N and 0 <= ty < N:
+                        xfly += matrix[tx][ty]
+            
+            if best < max(pfly, xfly):
+                best = max(pfly, xfly)
+    print(f'#{tc}', best)
