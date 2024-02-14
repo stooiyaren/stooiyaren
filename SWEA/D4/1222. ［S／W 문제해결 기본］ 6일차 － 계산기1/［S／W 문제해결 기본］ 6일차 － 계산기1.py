992a8e4c13e55f780971f9aef37330b324d898cc
stooiyaren <156271965+stooiyaren@users.noreply.github.com>
2024-02-14 13:52:28 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기1, Time: 172 ms, Memory: 58,524 KB -BaekjoonHub

--- SWEA/D4/1222. ［S／W 문제해결 기본］ 6일차 － 계산기1/［S／W 문제해결 기본］ 6일차 － 계산기1.py
+++ SWEA/D4/1222. ［S／W 문제해결 기본］ 6일차 － 계산기1/［S／W 문제해결 기본］ 6일차 － 계산기1.py
@@ -1,4 +1,57 @@
+def postfix(text):
+    stack = [] # push : append() / pop : pop()
+    result = ''
+    for i in text:
+        # (
+        if i == '(':
+            stack.append(i)
+        # */
+        elif i == '*' or i== '/':
+            # 스택에 들어있는 값이 * / 연산자라면 빼준다
+            if stack and (stack[-1] == '*' or stack[-1] == '/'):
+                result += stack.pop()
+            stack.append(i)            
+        # +-
+        elif i =='+' or i == '-':
+            # 여는 괄호가 나오거나 스택이 아예 빌 경우까지 pop을 계속 진행해준다
+            while stack and stack[-1] != '(':
+                result += stack.pop()
+            stack.append(i)
+        # )
+        elif i == ')':
+            # 여는 괄호가 나올 때까지 pop 해줘라
+            while stack[-1] != '(':                
+                result += stack.pop()
+            stack.pop()
+        # 숫자
+        elif i.isdigit():
+            result += i
+        # 스택을 모두 pop
+        
+    while stack:
+        result += stack.pop()
+    return result
+
+def middlewe(text):
+    stack = []
+    for i in text:
+        if i.isdigit():
+            stack.append(i)
+        
+        else:
+            b = int(stack.pop())
+            a = int(stack.pop())
+            if i == '*':
+                stack.append(a*b)
+            elif i == '/':
+                stack.append(a//b)
+            if i == '+':
+                stack.append(a+b)
+            if i == '-':
+                stack.append(a-b)
+    return stack[0]
+
 for tc in range(1,11):
-    N = int(input())
-    string = map(int,input().split('+'))
-    print(f'#{tc}', sum(string))
+    n = int(input())
+    text = input()
+    print(f'#{tc}', middlewe(postfix(text)))
