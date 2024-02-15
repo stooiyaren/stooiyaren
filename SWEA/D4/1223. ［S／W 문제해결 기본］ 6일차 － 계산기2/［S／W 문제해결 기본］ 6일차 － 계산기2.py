623ec8801337bbf579fb6655532d1dd273605590
stooiyaren <156271965+stooiyaren@users.noreply.github.com>
2024-02-15 10:07:30 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기2, Time: 154 ms, Memory: 58,248 KB -BaekjoonHub

--- SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.py
+++ SWEA/D4/1223. ［S／W 문제해결 기본］ 6일차 － 계산기2/［S／W 문제해결 기본］ 6일차 － 계산기2.py
@@ -1,57 +1,14 @@
-def postfix(text):
-    stack = [] # push : append() / pop : pop()
-    result = ''
-    for i in text:
-        # (
-        if i == '(':
-            stack.append(i)
-        # */
-        elif i == '*' or i== '/':
-            # 스택에 들어있는 값이 * / 연산자라면 빼준다
-            if stack and (stack[-1] == '*' or stack[-1] == '/'):
-                result += stack.pop()
-            stack.append(i)            
-        # +-
-        elif i =='+' or i == '-':
-            # 여는 괄호가 나오거나 스택이 아예 빌 경우까지 pop을 계속 진행해준다
-            while stack and stack[-1] != '(':
-                result += stack.pop()
-            stack.append(i)
-        # )
-        elif i == ')':
-            # 여는 괄호가 나올 때까지 pop 해줘라
-            while stack[-1] != '(':                
-                result += stack.pop()
-            stack.pop()
-        # 숫자
-        elif i.isdigit():
-            result += i
-        # 스택을 모두 pop
-        
-    while stack:
-        result += stack.pop()
-    return result
-
-def middlewe(text):
-    stack = []
-    for i in text:
-        if i.isdigit():
-            stack.append(i)
-        
+for tc in range(1, 11):
+    N = int(input())
+    n = list(input().split('+'))
+    for i in range(len(n)):
+        if len(n[i]) > 1:
+            cnt = 1
+            factors = list(map(int, n[i].split('*')))
+            for num in factors:
+                cnt *= num
+            n[i] = cnt
         else:
-            b = int(stack.pop())
-            a = int(stack.pop())
-            if i == '*':
-                stack.append(a*b)
-            elif i == '/':
-                stack.append(a//b)
-            if i == '+':
-                stack.append(a+b)
-            if i == '-':
-                stack.append(a-b)
-    return stack[0]
+            n[i] = int(n[i])
 
-for tc in range(1,11):
-    n = int(input())
-    text = input()
-    print(f'#{tc}', middlewe(postfix(text)))
+    print(f'#{tc}', sum(n))
