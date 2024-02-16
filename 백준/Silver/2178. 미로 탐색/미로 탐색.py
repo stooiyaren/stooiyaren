cd3551f179e415036558df8e8054f6b945fe616b
stooiyaren <156271965+stooiyaren@users.noreply.github.com>
2024-02-16 15:26:31 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,27 @@
+di = [-1,1,0,0]
+dj = [0,0,-1,1]
+
+def bfs(start, goal):
+    maze[start[0]][start[1]] = 0
+    q = deque()
+    q.append(start)    
+    while q:
+        start = q.popleft()
+
+        for i in range(4):
+            dx = start[0]+di[i]
+            dy = start[1]+dj[i]
+            if [dx,dy] == goal:
+                return start[2]+1
+            if 0 <= dx < N and 0 <= dy < M and maze[dx][dy] == 1:
+                q.append([dx,dy,start[2]+1])
+                maze[dx][dy] = 0
+    
+from collections import deque
+
+N, M = map(int,input().split())
+maze =[deque(map(int,input())) for _ in range(N)]
+
+S = [0,0,1]
+G = [N-1,M-1]
+print(bfs(S,G))
