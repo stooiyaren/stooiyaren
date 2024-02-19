35781b9e6f08b083dd0d185a4b291f7f36cfb725
stooiyaren <156271965+stooiyaren@users.noreply.github.com>
2024-02-19 22:05:31 +0900

    [D3] Title: 오목 판정, Time: 279 ms, Memory: 63,300 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.py
+++ SWEA/D3/11315. 오목 판정/오목 판정.py
@@ -1,50 +1,41 @@
-cro = [-1,1]
-ss = [-1,1]
-
-sip = [-1,1]
-ja = [1,-1]
-
 T = int(input())
-for tc in range(1,T+1):
+for tc in range(1, T + 1):
     N = int(input())
-    gb = [list(input().rstrip()) for _ in range(N)]
+    omok = [input() for _ in range(N)]
     ans = 0
-    
+
     for i in range(N):
         for j in range(N):
-            cnt1 =0
-            cnt2 =0
-            if gb[i][j] == 'o':
-                for k in range(2):
-                    for l in range(1,3):
-                        ni = i + cro[k]*l
-                        nj = j + ss[k]*l
-                        if 0<= ni < N and 0<= nj < N:
-                            if gb[ni][nj] == 'o':
-                                cnt1 += 1
-                       
-                        nx = i + sip[k]*l
-                        ny = j + ja[k]*l
-                        if 0<= nx < N and 0<= ny < N:
-                            if gb[nx][ny] == 'o':
-                                cnt2 += 1
-            if cnt1 ==4:
+            cnt1 = 0
+            cnt2 = 0
+
+            for k in range(5):
+                if 0<= (i+k) < N and 0<= (j+k) < N and omok[i+k][j+k] == 'o':
+                    cnt1 += 1
+
+                if 0<=(i-k)<N and 0<=(j+k)<N and omok[i-k][j+k] == 'o':
+                    cnt2 += 1
+            if cnt1 == 5:
                 ans = 1
-            if cnt2 ==4:
+                break
+            if cnt2 == 5:
                 ans = 1
-        
-    for i in gb:
-        a = ''.join(i)
-        if 'ooooo' in a:
+                break
+
+    for i in omok:
+        if 'ooooo' in i:
             ans = 1
-    gb = list(map(list,zip(*gb)))
+            break
+
+    omok = list(map(list,zip(*omok)))
     
-    for i in gb:
-        a = ''.join(i)
-        if 'ooooo' in a:
+    for i in omok:
+        i = ''.join(i)
+        if 'ooooo' in i:
             ans = 1
-        
-    if ans == 0:
-        print(f'#{tc} NO')
+            break
+
+    if ans:
+        print(f'#{tc}', 'YES')
     else:
-        print(f'#{tc} YES')
+        print(f'#{tc}', 'NO')
