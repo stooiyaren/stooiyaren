e97b6eb3ffb112e64268b018953de1c130ae20f3
stooiyaren <156271965+stooiyaren@users.noreply.github.com>
2024-02-27 23:56:07 +0900

    [D3] Title: 부분 수열의 합, Time: 3,795 ms, Memory: 62,508 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -1,15 +1,27 @@
+def f(x):
+    global N
+    global K
+    global ans
+    if x == N:
+        cnt = 0
+        for j in range(N):
+            if bit[j]:
+                cnt += n[j]
+        if cnt == K:
+            ans += 1
+            return
+
+    else:
+        bit[x] = 1
+        f(x+1)
+        bit[x] = 0
+        f(x+1)
+
 T = int(input())
 for tc in range(1,T+1):
     N, K = map(int,input().split())
-    A = list(map(int,input().split()))
-    a = len(A)
     ans = 0
-    
-    for i in range(1<<a):
-        cnt = 0
-        for j in range(a):
-            if i &(1<<j):
-                cnt += A[j]
-        if cnt == K:
-            ans += 1
-    print(f'#{tc} {ans}')
+    n = list(map(int,input().split()))
+    bit = [0] * N
+    f(0)
+    print(f'#{tc}', ans)
