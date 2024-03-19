249949fde73d6525e84e97d453d43a45cd13524c
stooiyaren <156271965+stooiyaren@users.noreply.github.com>
2024-03-19 10:10:30 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 124 ms, Memory: 43,784 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
@@ -1,25 +1,24 @@
+
 T = int(input())
-for tc in range(1, T+1):
-    daily, monthly, three, yearly = map(int,input().split())
-    plan = [0] + list(map(int,input().split())) + [0,0]
+for tc in range(1,T+1):
+    d,m,tm,y = map(int,input().split())
+    schedule = list(map(int,input().split()))
 
-    for i in range(1,13):
-        plan[i] *= daily
+    schedule = [0] + schedule # 0번째 달을 추가
 
-    for i in range(1,13):
-        if monthly < plan[i]:
-            plan[i] = monthly
-
-    d = [0] * 13
-    d[1] = plan[1]
-    d[2] = plan[1] + plan[2]
-    d[3] = min((plan[1] + plan[2] + plan[3]),three)
-    d[4] = min(d[3] + plan[4],plan[1] + three)
-    d[5] = min(d[4] + plan[5], plan[1]+plan[2] + three)
-    for i in range(6,13):
-        d[i] = min(d[i-1]+plan[i],d[i-3]+three)
-
-    if d[12] < yearly:
-        print(f'#{tc}', d[12])
+    for i in range(13): # 0달 + 12달 비교
+        if schedule[i]*d > m: # 일일 비용과 월 비용중 싼 가격으로 갱신
+            schedule[i] = m
+        else:
+            schedule[i] *= d
+    
+    # 세 달 비교
+    schedule[2] = schedule[1] + schedule[2]
+    for i in range(3,13):
+        schedule[i] = min(schedule[i]+schedule[i-1], tm + schedule[i-3])
+    
+    if schedule[12] > y:
+        ans = y
     else:
-        print(f'#{tc}', yearly)
+        ans = schedule[12]
+    print(f'#{tc} {ans}')
