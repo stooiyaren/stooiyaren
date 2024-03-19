21dfdfca6911bc77f7babe476a534fddf2b38925
stooiyaren <156271965+stooiyaren@users.noreply.github.com>
2024-03-19 14:14:12 +0900

    [Gold IV] Title: N-Queen, Time: 16888 ms, Memory: 156584 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,34 @@
+# N-퀸 문제
+
+# 중첩 함수(nested function):내부함수를 만들어서 간단하게 정리
+def n_queen_solve(N):
+
+    def dfs(depth):
+        nonlocal count, visited, board
+        if depth == N:
+            count += 1
+            return
+
+        for col in range(N):
+            if not visited[col] and check(depth, col):
+                board[depth] = col
+                visited[col] = True # 결정
+                dfs(depth +1)
+                visited[col] = False # 복구
+
+    def check(row,col):
+        for x in range(row):
+            y = board[x]
+            if abs(x-row) == abs(y-col):
+                return False
+        return True
+    
+    count = 0
+    visited = [False] * N
+    board = [-1]*N
+    dfs(0)
+
+    return count
+
+N = int(input()) # N 퀸의 N을 입력받아서
+print(n_queen_solve(N))
