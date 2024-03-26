5e79f32114ba8c5531d4abd3b00e913a78dc8232
stooiyaren <156271965+stooiyaren@users.noreply.github.com>
2024-03-26 14:54:26 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 152 ms, Memory: 110776 KB -BaekjoonHub

---
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int,input().split()))
+ar = arr[::-1]
+dp = [0]*N
+dpr = [0]*N
+
+for i in range(N):
+    cnt = 0
+    cnt2 = 0
+    for j in range(i):
+        if arr[i] > arr[j]:
+            if dp[j] > cnt:
+                cnt = dp[j]
+        if ar[i] > ar[j]:
+            if dpr[j] > cnt2:
+                cnt2 = dpr[j]
+    
+    dp[i] = cnt + 1
+    dpr[i] = cnt2 + 1
+
+ans = [0] *N
+for i in range(N):
+    ans[i] =dp[i]+dpr[N-i-1]
+print(max(ans)-1)
